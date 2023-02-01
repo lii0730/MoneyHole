94c16e54b4213ffe11ef1de9f084dd59b328e4ff
LeeHsss <lii0730@icloud.com>
2023-02-01 12:49:34 +0900

    Initial Commit

---
+++ MoneyHole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyHole
+//
+//  Created by LeeHsss on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
