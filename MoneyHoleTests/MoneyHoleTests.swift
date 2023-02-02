64652aefb8aae95c25a1a07cbb180c844d705062
LeeHsss <lii0730@icloud.com>
2023-02-02 18:07:11 +0900

    Unit Test 추가

---
+++ MoneyHoleTests/MoneyHoleTests.swift
@@ -0,0 +1,35 @@
+//
+//  MoneyHoleTests.swift
+//  MoneyHoleTests
+//
+//  Created by LeeHsss on 2023/02/02.
+//
+
+import XCTest
+@testable import MoneyHole
+
+final class MoneyHoleTests: XCTestCase {
+    
+    // 실행 전 세팅
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    // 실행 후 세팅
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testInsertCategory() throws {
+        
+    }
+    
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
