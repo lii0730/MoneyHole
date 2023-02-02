1e3c715d628273c9b6d539af337f104f900e42eb
LeeHsss <lii0730@icloud.com>
2023-02-02 18:03:40 +0900

    Date Extension 작성

---
+++ MoneyHole/Extension/Date+.swift
@@ -0,0 +1,28 @@
+//
+//  Date+.swift
+//  MoneyHole
+//
+//  Created by LeeHsss on 2023/02/02.
+//
+
+import Foundation
+
+
+extension Date {
+    var year: String {
+        let dateComponents = Calendar.current.dateComponents([.year], from: self)
+        return "\(dateComponents.year!)"
+    }
+    
+    var yearMonth: String {
+        let dateComponents = Calendar.current.dateComponents([.year, .month], from: self)
+        return "\(dateComponents.year!)-\(dateComponents.month!)"
+    }
+    
+    var dateString: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        return formatter.string(from: self)
+    }
+}
