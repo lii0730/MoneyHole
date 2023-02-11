dad2b4d8c888c896f9685291e8344b07d5da7c31
LeeHsss <lii0730@icloud.com>
2023-02-11 18:53:08 +0900

    20230211 테스트 코드 수정

--- MoneyHoleTests/MoneyHoleTests.swift
+++ MoneyHoleTests/MoneyHoleTests.swift
@@ -10,26 +10,17 @@ import XCTest
 
 final class MoneyHoleTests: XCTestCase {
     
+    var sut: DBUtil!
+    
     // 실행 전 세팅
     override func setUpWithError() throws {
         try super.setUpWithError()
+        sut = DBUtil()
     }
 
     // 실행 후 세팅
     override func tearDownWithError() throws {
         try super.tearDownWithError()
+        sut = nil
     }
-
-    func testInsertCategory() throws {
-        
-    }
-    
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
